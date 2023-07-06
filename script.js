27386c537662d8635a9fdbde92affc7954e4cc81
Dilip Kumar r <99712699+dilipkumar116@users.noreply.github.com>
2023-07-06 21:42:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let outputScreen = document.getElementById("output-screen");
+
+function display(num){
+    outputScreen.value += num;
+}
+
+function calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(error){
+        alert("invalid")
+    }
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
